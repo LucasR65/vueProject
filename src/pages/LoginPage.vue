6c1a091a312ff3bcba2654527cb4b1d8fcef277e
LucasR65 <158263593+LucasR65@users.noreply.github.com>
2025-02-12 16:46:14 +0100

    Initial commit

---
+++ src/pages/LoginPage.vue
@@ -0,0 +1,59 @@
+<template>
+    <main class="login__main">
+      <h1>LoginPage</h1>
+      <form @submit.prevent.stop.once="submitHandler">
+        <section>
+          <article>
+            <label for="email">Email</label>
+            <input
+              v-model="data.email"
+              type="email"
+              id="email"
+              placeholder="votre email"
+            />
+          </article>
+          <article>
+            <label for="password">Mot de passe</label>
+            <input
+              v-model="data.password"
+              type="password"
+              id="paswword"
+              placeholder="votre mot de passe"
+            />
+          </article>
+        </section>
+        <section>
+          <button type="submit">Se connecter</button>
+          <button type="reset">Réintialiser</button>
+        </section>
+      </form>
+    </main>
+  </template>
+  
+  <script setup lang="ts">
+  import { watch, reactive } from "vue";
+  
+  const data = reactive({
+    email: "",
+    password: "",
+  });
+  
+  watch(data, (val) => {
+    console.log(val);
+  });
+  
+  const isUserInputValid = (input: string): boolean => {
+    const pattern = new RegExp(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/);
+    return pattern.test(input);
+  };
+  
+  const submitHandler = () => {
+    if (isUserInputValid(data.email)) {
+      console.log("Email est valide");
+    } else {
+      console.log("Email pas valide");
+    }
+  };
+  
+  const inputHandler = () => {};
+  </script>
